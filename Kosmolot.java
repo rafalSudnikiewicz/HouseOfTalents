2325cba4d48db7ee035c8a8938f63bfa70744e22
Rafal Sudnikiewicz <r.sudnikiewicz@gmail.com>
2019-01-27 11:52:09 +0100

    Rocket with shield finished

--- Kosmolot.java
+++ Kosmolot.java
@@ -4,6 +4,9 @@ public class Kosmolot {
     private static final int PARAMOFTSHIELD = 1;
     private static final char FILLED = '*';
     private static final char BLANK = ' ';
+    private static final char ENGINEORHEAD = '>';
+    private static final char LEFTSHIELD = '\\';
+    private static final char RIGTHSHIELD = '/';
 
 
     public static void main(String[] args) {
@@ -15,9 +18,11 @@ public class Kosmolot {
 
         try {
             size = Integer.parseInt(args[PARAMOFSIZE]);
-            if (args[PARAMOFTSHIELD].toUpperCase().equals("Y")) shieldsOn = true;
-            if (args[PARAMOFTSHIELD].toUpperCase().equals("N")) shieldsOn = false;
-            else return;
+            if (args[PARAMOFTSHIELD].equals("Y")) {
+                shieldsOn = true;
+            } else if (args[PARAMOFTSHIELD].equals("N")) {
+                shieldsOn = false;
+            } else return;
         } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
             return;
         }
@@ -34,15 +39,22 @@ public class Kosmolot {
     }
 
     private static void printRocketWithShield(int size) {
-//        for (int i = 0; i < 2 * size - 1; i++) {
-//            for (int j = 0; j < size * size; j++) {
-//                if ((j % size <= i && i < size) || (j % size < size - i % size - 1 && i >= size)) {
-//                    System.out.print(FILLED);
-//                } else System.out.print(BLANK);
-//
-//            }
-//            System.out.println();
-//        }
+        for (int i = 0; i < 2 * size - 1; i++) {
+            for (int j = 0; j < size * size; j++) {
+                if ((j % size <= i && i < size) || (j % size < size - i % size - 1 && i >= size)) {
+                    
+                    if (j == 0 || (i == size - 1 && j == size * size - 1)) {
+                        System.out.print(ENGINEORHEAD);
+                    } else if (j % size == i && i < size - 1) {
+                        System.out.print(LEFTSHIELD);
+                    } else if (j % size == size - i % size - 2 && i >= size) {
+                        System.out.print(RIGTHSHIELD);
+                    } else System.out.print(FILLED);
+                } else System.out.print(BLANK);
+
+            }
+            System.out.println();
+        }
     }
 
     private static void printRocketWithoutShield(int size) {
